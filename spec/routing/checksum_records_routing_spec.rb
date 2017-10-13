c3b3d9dc932d554c2093cfcefd69f633cbeb77f1
Naomi Dushay <ndushay@stanford.edu>
2017-10-12 17:38:42 -0700

    checksum_recourds_routing_spec: avoid rocket hash symbol

--- spec/routing/checksum_records_routing_spec.rb
+++ spec/routing/checksum_records_routing_spec.rb
@@ -4,30 +4,27 @@ RSpec.describe ChecksumRecordsController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/checksum_records").to route_to("checksum_records#index")
+      expect(get: "/checksum_records").to route_to("checksum_records#index")
     end
-
 
     it "routes to #show" do
-      expect(:get => "/checksum_records/1").to route_to("checksum_records#show", :id => "1")
+      expect(get: "/checksum_records/1").to route_to("checksum_records#show", id: "1")
     end
 
-
     it "routes to #create" do
-      expect(:post => "/checksum_records").to route_to("checksum_records#create")
+      expect(post: "/checksum_records").to route_to("checksum_records#create")
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/checksum_records/1").to route_to("checksum_records#update", :id => "1")
+      expect(put: "/checksum_records/1").to route_to("checksum_records#update", id: "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/checksum_records/1").to route_to("checksum_records#update", :id => "1")
+      expect(patch: "/checksum_records/1").to route_to("checksum_records#update", id: "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/checksum_records/1").to route_to("checksum_records#destroy", :id => "1")
+      expect(delete: "/checksum_records/1").to route_to("checksum_records#destroy", id: "1")
     end
-
   end
 end
